247abe84321c22926e6d54f23fc61096ad105208
Troy Miles <rockncoder@gmail.com>
2014-07-16 11:05:38 -0700

    added attributes to hold min, max, and start values

--- Starts/Widget/app.js
+++ Starts/Widget/app.js
@@ -2,6 +2,48 @@
 
 var app = angular.module('myApp', []);
 
-app.controller('MainCtrl', function ($scope) {
-});
+app.controller('MainCtrl', ['$scope', function ($scope) {
+  $scope.currentSliderValue = 0;
+  $scope.start = 5;
+  $scope.stop = 20;
+}]);
+
+
+app.directive('rncSliderRange', [function(){
+  // one time initialization code here
+  var startTime = new Date();
+
+
+  // define the configuration object
+  return {
+    restrict: 'A',
+    //require: '?ngModel',
+    link: function($scope, $element, $attr, ngModel) {
+      var initialized = false,
+        update = function(currentValue){
+          $scope.$apply(function(){
+            $scope.currentSliderValue = currentValue;
+          });
+        };
+
+      setTimeout(function(){
+        initialized = $element.slider().
+          on('slide', function(event, ui){
+            console.log("Current position = " +  ui.value);
+            // let Angular know the new slider position
+            update(ui.value);
+          });
+
+        $element.slider({
+          value: parseInt($attr.start, 10),
+          min: parseInt($attr.min, 10),
+          max: parseInt($attr.max, 10)
+        });
+
+        var temp = $element.slider( "value" );
+        update(temp);
+      });
+    }
+  };
+}]);
 
